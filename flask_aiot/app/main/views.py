70a77dc4908db940c1d94045ad02f1dfeefe3625
Gladysgong <531404376@qq.com>
2017-08-28 17:15:38 +0800

    views

--- flask_aiot/app/main/views.py
+++ flask_aiot/app/main/views.py
@@ -1,6 +1,10 @@
 from flask import Blueprint,request,render_template
 from flask.views import MethodView
 from ..models import Movies
+from ..models import Chinacwa
+# from ..models import Iot
+# from ..models import Ny135
+
 from . import main
 from datetime import datetime
 
@@ -13,8 +17,17 @@ from datetime import datetime
 def index():
     return render_template('index.html')
 
-@main.route('/showall/',methods=['GET'])
-def ShowAllView():
-    movies = Movies.objects.all()
-    return render_template('list.html',movies=movies)
+@main.route('/show_chinacwa/',methods=['GET'])
+def ShowChinacwaView():
+    chinacwa = Chinacwa.objects.all()
+    return render_template('list.html',articles=chinacwa)
 
+# @main.route('/show_iot/',methods=['GET'])
+# def ShowIotcwaView():
+#     iot = Iot.objects.all()
+#     return render_template('list.html',articles=iot)
+#
+# @main.route('/show_ny135/',methods=['GET'])
+# def ShowNy135cwaView():
+#     ny135 = Ny135.objects.all()
+#     return render_template('list.html',articles=ny135)
