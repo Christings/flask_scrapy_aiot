f588764950dc15492320c790f05a7d1ae1d5a410
Gladysgong <531404376@qq.com>
2017-08-29 15:05:06 +0800

    __init__

--- flask_aiot/app/__init__.py
+++ flask_aiot/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mongoengine import MongoEngine
 from config import config
@@ -8,12 +8,11 @@ from config import config
 # from  import config
 
 bootstrap = Bootstrap()
-# db = SQLAlchemy()
+db = SQLAlchemy()
 mongo = MongoEngine()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
-
 
 def create_app(config_name):
     app = Flask(__name__)
