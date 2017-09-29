fb4cc69e1d128c6de8c3b5352d09b1db5b4ad071
Gladysgong <531404376@qq.com>
2017-09-29 14:01:35 +0800

    修改app的init文件

--- flask_aiot/app/__init__.py
+++ flask_aiot/app/__init__.py
@@ -14,6 +14,7 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
@@ -22,7 +23,12 @@ def create_app(config_name):
     # db.init_app(app)
     mongo.init_app(app)
     login_manager.init_app(app)
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
 
     return app
