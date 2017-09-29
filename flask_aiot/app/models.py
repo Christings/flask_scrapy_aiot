4403c9bb5f6e64422e7500aab16fddd9a2ba5a4f
Gladysgong <531404376@qq.com>
2017-09-29 14:00:50 +0800

    在models中增加User

--- flask_aiot/app/models.py
+++ flask_aiot/app/models.py
@@ -1,4 +1,8 @@
-from . import mongo
+from . import mongo, login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 
 
 class Chinacwa(mongo.Document):
@@ -41,6 +45,63 @@ class AllProductPrice(mongo.Document):
     product_market = mongo.StringField(required=True)
     product_releasedate = mongo.StringField(required=True)
     meta = {'collection': 'allproductprice'}
+
+
+class User(UserMixin, mongo.Document):
+    # uid = mongo.IntField(requires=True)
+    email = mongo.StringField(max_length=255, requires=True)
+    username = mongo.StringField(max_length=255, requires=True)
+    # password = mongo.StringField(requires=True)
+    password_hash = mongo.StringField(requires=True)
+
+    # confirmed=mongo.BooleanField(default=False)
+
+    # password_hash = mongo.StringField(requires=True)
+    # confirmed = mongo.BooleanField(requires=True)
+    # meta = {'collection': 'user'}
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def get_id(self):
+        try:
+            # return unicode(self.username)
+            return self.username
+        except AttributeError:
+            raise NotImplementedError('No `username` attribute - override `get_id`')
+
+    def __unicode__(self):
+        return self.username
+
+        # def confirm(self,token):
+        #     s=Serializer(current_app.config['SECRET_KEY'])
+        #     try:
+        #         data=s.loads(token)
+        #     except:
+        #         return False
+        #     if data.get('confirm')!=self.username
+        #         return False
+        #     self.confirmd=True
+
+
+@login_manager.user_loader
+def load_user(username):
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        user = None
+    return user
 
 
 class Movies(mongo.Document):
