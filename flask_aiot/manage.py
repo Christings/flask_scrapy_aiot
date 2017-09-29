8fef213368a5c0eb256e973644371070a4220cc1
Gladysgong <531404376@qq.com>
2017-09-29 14:02:26 +0800

    修改manage中的bug

--- flask_aiot/manage.py
+++ flask_aiot/manage.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python
 import os
 # from app import create_app, db
-from app import create_app
+from app import create_app, mongo
+from app.models import User, AllProductPrice, Ny135, Chinacwa, Iot
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
-from app.models import Movies
+
+# from app.models import Movies
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
-# migrate = Migrate(app, db)
+migrate = Migrate(app, mongo)
+
 
 def make_shell_context():
-    return dict(app=app, db=db, Person_Meizi=Movies)
+    return dict(app=app, db=mongo, User=User, AllProductPrice=AllProductPrice,
+                Ny135=Ny135, Chinacwa=Chinacwa, Iot=Iot)
 
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 
 if __name__ == '__main__':
